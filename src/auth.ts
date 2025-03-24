422b26bc4246e4ea11ff16362283fb11c0c5ccdc
phofic <jastus059@gmail.com>
2025-03-24 01:23:12 -0400

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,22 +1,24 @@
-import NextAuth from "next-auth"
-import authConfig from "./auth.config"
-import prisma from "@/app/prismadb"
-import { PrismaAdapter } from "@auth/prisma-adapter"
- 
- 
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import prisma from "@/app/prismadb";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  callbacks:{
-    async jwt({token,user}){
-      return token
-    },
-    async session({token,session}){
-      if(session.user&& token.sub){
-        session.user.id = token.sub
-      }
-      return session
-    }
-  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.sub = user.id; // Add user ID to the token
+      }
+      return token;
+    },
+    async session({ token, session }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub; // Add user ID to the session
+      }
+      return session;
+    },
+  },
   ...authConfig,
-})
+});
