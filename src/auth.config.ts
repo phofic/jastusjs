154570ba1e3b9abafe6e1978fa91179342c73b22
phofic <jastus059@gmail.com>
2025-03-24 01:22:16 -0400

    Update auth.config.ts

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,37 +1,35 @@
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
-import Credentials from "next-auth/providers/credentials"
-import type { NextAuthConfig } from "next-auth"
-import { loginSchema } from "./Schema/LoginSchema"
-import prisma from "@/app/prismadb"
-import bcrypt from "bcryptjs"
- 
-// Notice this is only an object, not a full Auth.js instance
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import type { NextAuthConfig } from "next-auth";
+import { loginSchema } from "./Schema/LoginSchema";
+import prisma from "@/app/prismadb";
+import bcrypt from "bcryptjs";
+
 export default {
   providers: [
     GitHub({
-        clientId:process.env.GITHUB_CLIENT_ID,
-        clientSecret:process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     Google({
-        clientId:process.env.GOOGLE_CLIENT_ID,
-        clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     Credentials({
-        async authorize(credentials){
-            const validateData = loginSchema.safeParse(credentials)
-            if(!validateData.success) return null
-            const {email, password} = validateData.data
-            const user = await prisma.user.findUnique({where:{email}})
-            if(!user || !user.password){
-                return null
-            }
+      async authorize(credentials) {
+        const validateData = loginSchema.safeParse(credentials);
+        if (!validateData.success) return null;
 
-            const passwordmatch = await bcrypt.compare(password,user.password)
+        const { email, password } = validateData.data;
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user || !user.password) return null;
 
-            if(passwordmatch) return user
-            return null
-        }
-    })
-],
-} satisfies NextAuthConfig
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) return null;
+
+        return user;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
